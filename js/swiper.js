3a38fb1eae76e8c66bde9526d105abd1943a9d27
Kamil Kalbarczyk <k.kalbarczyk1@gmail.com>
2021-10-29 17:01:43 +0200

    add slider using swiper js

---
+++ js/swiper.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper(".mySwiper", {
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+    },
+    loop: true,
+    grabCursor: true,
+    simulateTouch: true,
+});
